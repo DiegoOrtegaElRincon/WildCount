28104ddeacb126b217b1aa141fc3f3b117bcc95d
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2024-03-07 10:46:33 +0100

    Startup page

--- client/src/pages/startup/Startup.jsx
+++ client/src/pages/startup/Startup.jsx
@@ -1,29 +1,41 @@
+// Startup.jsx
+
 import React, { useState } from 'react';
 import LoginModal from '../../components/loginModal/LoginModal';
+import './startup.scss';
 
 const Startup = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return (
-    <div className='h-[100vh] relative'>
-      {/* Bottom line on the left side */}
-      <span className='bg-black h-[2rem] w-[50vw] absolute top-[57.5%] -translate-y-1/2 left-0'></span>
-      {/* Circle in the middle */}
-      <span
-        className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 bg-white h-[15rem] w-[15rem] rounded-full border-[12px] border-black'
-        onClick={() => setIsModalOpen(true)}
-      ></span>
-      {/* Line in the circle in the middle */}
-      <span
-        className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-20 bg-black h-[2.4rem] w-[14.8rem] -rotate-[40deg]'
-        onClick={() => setIsModalOpen(true)}
-      ></span>
-      {/* Top line on the right side */}
-      <span className='bg-black h-[2rem] w-[50vw] absolute top-[42.5%] -translate-y-1/2 right-0'></span>
-      
-      <LoginModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
-    </div>
-  );
+    const handleLogin = () => {
+        const middleCircle = document.getElementById('middle-circle');
+        middleCircle.classList.add('rotate-animation'); // Add keyframe animation class
+        const leftLine = document.getElementById('left-line');
+        leftLine.classList.add('move-right-animation'); // Add keyframe animation class
+        const rightLine = document.getElementById('right-line');
+        rightLine.classList.add('move-left-animation'); // Add keyframe animation class
+
+        setTimeout(() => {
+            window.location.href = '/home'; // Redirect to homepage after animation
+        }, 1000); // Adjust the delay to match your keyframe animation duration
+    };
+
+    return (
+        <div className='h-[100vh] relative'>
+            {/* Bottom line on the left side */}
+            <span id='left-line' className='bg-black h-[2rem] w-[50vw] absolute top-[57.5%] -translate-y-1/2 left-0'></span>
+            <div id='middle-circle' className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10'>
+                {/* Circle in the middle */}
+                <span className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white h-[15rem] w-[15rem] rounded-full border-[12px] border-black cursor-pointer' onClick={() => setIsModalOpen(true)}></span>
+                {/* Line in the circle in the middle */}
+                <span className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-black h-[2.4rem] w-[14.8rem] -rotate-[40deg] cursor-pointer' onClick={() => setIsModalOpen(true)}></span>
+            </div>
+            {/* Top line on the right side */}
+            <span id='right-line' className='bg-black h-[2rem] w-[50vw] absolute top-[42.5%] -translate-y-1/2 right-0'></span>
+
+            <LoginModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} onLogin={handleLogin} />
+        </div>
+    );
 };
 
 export default Startup;
