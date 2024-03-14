95754da61c6febb35cfced81365af466c745383e
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2024-03-14 09:33:40 +0100

    Update EndangeredComponent.tsx

--- client/src/components/EndangeredComponent/EndangeredComponent.tsx
+++ client/src/components/EndangeredComponent/EndangeredComponent.tsx
@@ -16,13 +16,16 @@ const AnimalModal = ({ animal, onClose }: { animal: Animal, onClose: () => void 
     if (!animal) return null;
 
     return (
-        <div className='fixed top-0 left-0 right-0 bottom-0 bg-[#00000005] flex justify-center items-center'>
-            <div className='bg-white p-5 rounded-[5px] max-w-[80%] max-h-[80%] overflow-auto'>
-                <button onClick={onClose} className='float-end'>X</button>
-                <img src={animal.image || placeholder_image} alt={`Image of ${animal.name}`} className='max-w-[250px] max-h-full' />
-                <h3>Name: {animal.name}</h3>
-                <p>Description: {animal.description}</p>
-                <p><strong>Alive:</strong> {animal.amount_left}</p>
+        <div className='fixed top-0 left-0 right-0 bottom-0 bg-[#00000099] flex justify-center items-center'>
+            <div className='relative bg-white p-5 rounded-[10px] max-w-[100%] max-h-[100%] overflow-auto'>
+                <button onClick={onClose} className='absolute top-2 right-2 z-50 bg-[#fff] h-[40px] w-[40px] rounded-full'>
+                    <span className='h-1 w-7 bg-black block rotate-45 translate-y-1 rounded-lg m-auto'></span>
+                    <span className='h-1 w-7 bg-black block -rotate-45 rounded-lg m-auto'></span>
+                </button>
+                <img src={animal.image || placeholder_image} alt={`Image of ${animal.name}`} className='max-w-[250px] max-h-full rounded-lg m-auto' />
+                <p className='font-bold'>Name: <span className='font-normal underline'>{animal.name}</span></p>
+                <p className='font-bold'>Description: <span className='font-normal'>{animal.description}</span></p>
+                <p className='font-bold'>Alive: <span className='font-normal underline'>{animal.amount_left}</span></p>
             </div>
         </div>
     );
@@ -48,16 +51,16 @@ const EndangeredComponent = () => {
     };
 
     return (
-        <div className='flex flex-wrap justify-around items-center pr-[75px]'>
-            <div className='grid grid-cols-3'>
-            {animals.map((animal, index) => (
-                <div key={index} className='flex flex-col'>
-                        <button className='bg-none border-none p-0 cursor-pointer' onClick={() => handleAnimalClick(animal)}>
-                            <img src={animal.image || placeholder_image} alt={`Image of ${animal.name}`} className='w-[250px] h-auto object-contain' />
+        <div className='flex flex-wrap justify-around items-center pb-[75px] lg:pb-0 lg:pr-[75px] my-8 mx-8'>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-16'>
+                {animals.map((animal, index) => (
+                    <div key={index} className='flex flex-col justify-between items-center'>
+                        <button className='bg-none border-none p-0 cursor-pointer flex items-center h-full' onClick={() => handleAnimalClick(animal)}>
+                            <img src={animal.image || placeholder_image} alt={`Image of ${animal.name}`} className='w-[250px] h-auto object-contain rounded-3xl' />
                         </button>
-                        <p><strong>Alive:</strong> {animal.amount_left}</p>
+                        <p className='font-bold mt-4'>Alive: {animal.amount_left}</p>
                     </div>
-            ))}
+                ))}
             </div>
             {isModalOpen && <AnimalModal animal={selectedAnimal!} onClose={() => setIsModalOpen(false)} />}
         </div>
