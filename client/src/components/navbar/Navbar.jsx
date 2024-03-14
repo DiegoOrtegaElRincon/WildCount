be045af9f1a8072381864fe269eff432b30a433d
dirbirb <ingvirb@gmail.com>
2024-03-14 12:41:03 +0000

    changes to user page and user.scss & startup.scss

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { navLinks } from '../../constants';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { navLinks } from "../../constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
-    return (
-        <nav className='fixed bottom-0 lg:right-0 bg-[#454545] text-white w-full lg:w-auto lg:h-full flex justify-evenly items-center lg:flex-col mt-[200px]'>
-            {navLinks.map((navLink) => (
-                <NavLink
-                    to={navLink.link}
-                    key={navLink.id}
-                    className={({ isActive }) => isActive ? 'text-4xl p-4 active-link' : 'text-4xl p-4 hover:text-[#cccccc]'}
-                >
-                    <FontAwesomeIcon icon={navLink.icon} />
-                </NavLink>
-            ))}
-        </nav>
-    )
-}
+  return (
+    <nav className="fixed bottom-0 lg:right-0 bg-[#454545] text-white w-full lg:w-auto lg:h-full flex justify-evenly items-center lg:flex-col mt-[200px] z-10">
+      {navLinks.map((navLink) => (
+        <NavLink
+          to={navLink.link}
+          key={navLink.id}
+          className={({ isActive }) =>
+            isActive
+              ? "text-4xl p-4 active-link"
+              : "text-4xl p-4 hover:text-[#cccccc]"
+          }
+        >
+          <FontAwesomeIcon icon={navLink.icon} />
+        </NavLink>
+      ))}
+    </nav>
+  );
+};
 
 export default Navbar;
